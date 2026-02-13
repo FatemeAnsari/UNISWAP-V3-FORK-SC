8024462ba6f0799764b90edc5eb55d040740b1c8
fatemeAnsari <fateme_ansarii@yahoo.com>
2026-02-14 00:19:59 +0330

    v3core files

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,58 +1,68 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-ignition-ethers";
-import "@nomicfoundation/hardhat-verify";
-import 'dotenv/config';
+import 'hardhat-typechain'
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-waffle'
+import '@nomiclabs/hardhat-etherscan'
 
-const PRIVATE_KEY= process.env.PRIVATE_KEY as string;
-
-const tBNB_RPC_URL = process.env.tBNB_RPC_URL;
-
-const tBNB_API_KEY = process.env.tBNB_API_KEY || '';
-
-const config: HardhatUserConfig = {
-  solidity: {
-    compilers: [
-      {
-        version: "0.7.6",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-            details: {
-              yul: true
-            }
-          },
-          viaIR : false,
-        },
-      },
-      {
-        version: "0.4.22"
-      }
-    ]
-  },
-  paths: {
-    sources: "./src"
-  },
+export default {
   networks: {
-    tBNB: {
-      url: tBNB_RPC_URL,
-      accounts: [PRIVATE_KEY],
-    }
+    hardhat: {
+      allowUnlimitedContractSize: false,
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    arbitrumRinkeby: {
+      url: `https://arbitrum-rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    arbitrum: {
+      url: `https://arbitrum-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    optimismKovan: {
+      url: `https://optimism-kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    optimism: {
+      url: `https://optimism-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    polygon: {
+      url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    bnb: {
+      url: `https://bsc-dataseed.binance.org/`,
+    },
   },
   etherscan: {
-    apiKey: {
-      tBNB: tBNB_API_KEY
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  solidity: {
+    version: '0.7.6',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 800,
+      },
+      metadata: {
+        // do not include the metadata hash, since this is machine dependent
+        // and we want all generated code to be deterministic
+        // https://docs.soliditylang.org/en/v0.7.6/metadata.html
+        bytecodeHash: 'none',
+      },
     },
-    customChains: [{
-      network: "tBNB",
-      chainId: 97,
-      urls: {
-        apiURL: "https://api-amoy.polygonscan.com/api",
-        browserURL: "https://testnet.bscscan.com/"
-      }
-    }]
-  }
-};
-
-export default config;
+  },
+}
